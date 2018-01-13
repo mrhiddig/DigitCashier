011879c68d15b03ca6ba77568579329d621c6788
fulsomenko <max.blomstervall@gmail.com>
2018-01-13 23:48:02 +0100

    Added FileHandler class. Moved the start point out of program.cs. Login protype.

---
+++ DigitCashier/CashRegister/runApp.cs
@@ -0,0 +1,39 @@
+﻿using CashRegister.filehandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister
+{
+    class runApp
+    {
+        public static void Main(string[] args)
+        {
+            FileHandler fh = new FileHandler("Products.txt");
+            fh.addLine("Super_Product 1 0 yolo");
+            
+            String code;
+            Program program;
+
+            Console.WriteLine("Please log in with your three digit code: ");
+            code = Console.ReadLine();
+
+            if (code.Length > 0) { 
+                if (code[0].Equals('2'))
+                {
+                    program = new Program();
+                }
+                else if (code[0].Equals('3'))
+                {
+
+                }
+                else if (code[0].Equals('5'))
+                {
+
+                }
+            }
+        }
+    }
+}

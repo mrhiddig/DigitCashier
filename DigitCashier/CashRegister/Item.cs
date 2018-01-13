799e8078165259374c0b6e896500ea5803a7bdbb
mrhiddig <35103301+mrhiddig@users.noreply.github.com>
2018-01-13 17:55:39 +0100

    New Bugatti
    ye ye

--- DigitCashier/CashRegister/Item.cs
+++ DigitCashier/CashRegister/Item.cs
@@ -1,22 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CashRegister
+﻿namespace CashRegister
 {
-    //I created an Abstract Class and named it Item
-    abstract class Item
+    public class Item
     {
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public int Quantity { get; set; }
-        public int InStock { get; set; }
-        public int Total { get; set; }
-        public string ItemCode { get; set; }
-        public abstract string HowManyItemsOfChoice();
-        public abstract string Receipt();
-        //public abstract string PrintUserChoicesToTextFile();
     }
 }
